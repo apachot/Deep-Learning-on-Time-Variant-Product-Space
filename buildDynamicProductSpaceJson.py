c8b18853e5204a2cb1cc89783af8c8d9a0ddea46
Arnault Pachot <apachot@openstudio.fr>
2022-10-31 06:11:53 +0100

    normalizing data in export files and fixe issues in dynamic product space in json generation

--- buildDynamicProductSpaceJson.py
+++ buildDynamicProductSpaceJson.py
@@ -17,31 +17,7 @@ mynode_ids = pd.DataFrame(product_id, index=hs_product_code).to_dict()[0]
 #print(mynode_ids)
 
 
-data = pd.read_csv('./input/proximities/HS6_Proximities_0.55_2019.csv')
-del data['weight']
-myedges = data.values.tolist()
-for k in range(0, len(myedges)):
-	i = myedges[k][0]
-	j = myedges[k][1]
-	
-	if(i!='unspecified' and i!='travel' and i!='transport' and i!='financial' and i!='ict'):
-		i = str(i).zfill(6)
-	if(j!='unspecified' and j!='travel' and j!='transport' and j!='financial' and j!='ict'):
-		j = str(j).zfill(6)
-	#print("i",i,"j",j)
 
-	index_i = hs_product_code.index(i)
-	index_j = hs_product_code.index(j)
-	code_i = product_id[index_i]-5000
-	code_j = product_id[index_j]-5000
-	if code_i >= 6000:
-		code_i = code_i - 961
-	if code_j >= 6000:
-		code_j = code_j - 961
-
-	myedges[k][0] = code_i
-	myedges[k][1] = code_j
-	#print("index_i",code_i,"index_j",code_j)
 	
 #print("len(product_id)", len(product_id))
 #for i,j in myedges:
@@ -54,6 +30,32 @@ country="FRA"
 dictionary = {}
 for k in range(0, 26):
 
+	data = pd.read_csv('./input/proximities/HS6_Proximities_0.55_'+str(1995+k)+'.csv')
+	del data['weight']
+	myedges = data.values.tolist()
+	for k2 in range(0, len(myedges)):
+		i = myedges[k2][0]
+		j = myedges[k2][1]
+	
+		if(i!='unspecified' and i!='travel' and i!='transport' and i!='financial' and i!='ict'):
+			i = str(i).zfill(6)
+		if(j!='unspecified' and j!='travel' and j!='transport' and j!='financial' and j!='ict'):
+			j = str(j).zfill(6)
+		#print("i",i,"j",j)
+
+		index_i = hs_product_code.index(i)
+		index_j = hs_product_code.index(j)
+		code_i = product_id[index_i]-5000
+		code_j = product_id[index_j]-5000
+		if code_i >= 6000:
+			code_i = code_i - 961
+		if code_j >= 6000:
+			code_j = code_j - 961
+
+		myedges[k2][0] = code_i
+		myedges[k2][1] = code_j
+	#print("index_i",code_i,"index_j",code_j)
+
 	# weights
 	data = pd.read_csv('./input/proximities/HS6_Proximities_0.55_'+str(1995+k)+'.csv')
 	del data['target']
@@ -65,35 +67,40 @@ for k in range(0, 26):
 	#for country in country_list:
 	print("country", country)
 	print("k", k)
-	myFX = []
-	for year in range(1995, 2021):
-		data = pd.read_csv('./input/exports/'+country+'/exports_'+country+'_'+str(year)+'.csv')
-		myFXtmp = data['export_value'].values.tolist()
-		myFXtmp = [0 if math.isnan(x) else x for x in myFXtmp]
-		#print("longeur=", len(myFXtmp))
-		myFX.append(myFXtmp)
-
+	#for year in range(1995, 2021):
+	data = pd.read_csv('./input/exports/'+country+'/exports_'+country+'_'+str(1995+k)+'.csv')
+	myFX = data['export_value'].values.tolist()
+	myFY = myFX
+	myFX = [[0] if math.isnan(x) else [x] for x in myFX]
+	myFY = [0 if math.isnan(x) else x for x in myFY]
+	#print("longeur=", len(myFXtmp))
+	
 
 	#print("myFX", myFX)
-	myFX = preprocessing.normalize(myFX, axis=0)
-	myFX = myFX.tolist()
+	#myFX = preprocessing.normalize(myFX, axis=1)
+	#myFX = myFX.tolist()
 
-	myFX_x = myFX[0:-1]
-	myFX_y = myFX[1:]
+	#myFX_x = myFX[0:-1]
+	#myFX_y = myFX[1:]
 	
-	print("myFX_x", len(myFX_x))
-	print("myFX_y", len(myFX_y))
+	#print("myFX_x", len(myFX_x))
+	#print("myFX_y", len(myFX_y))
+	print("myFX.len", len(myFX))
+	print("myedges.len", len(myedges))
+	print("myweights.len", len(myweights))
+
 
 
 
 	dictionary[k]={"index": k,
 	    "edges": myedges,
    	 	"weights": myweights,
-    	"X": myFX_x,
-    	"y": myFX_y}
+    	"y": myFY,
+    	"X": myFX
+    	}
 	
 
-dictionary["time_periods"] = 24
+dictionary["time_periods"] = 26
 dictionary["node_ids"] = mynode_ids
 #json_name = "./dataset/dynamic_productspace_"+country+".json"
 json_name = "./dataset/dynamic_productspace.json"
--- extractExportsByCountries.py
+++ extractExportsByCountries.py
@@ -1,6 +1,27 @@
 import pandas as pd
 import os
-data = pd.io.stata.read_stata('input/country_hsproduct6digit_year.dta')
+import math
+from sklearn import preprocessing
+dta_file = 'input/country_hsproduct6digit_year.dta'
+print("Loading", dta_file,"...")
+
+data = pd.io.stata.read_stata(dta_file)
+print("Normalization...")
+
+data['export_value'] = [0 if math.isnan(x) else x for x in data['export_value']]
+data['import_value'] = [0 if math.isnan(x) else x for x in data['import_value']]
+
+frames = pd.DataFrame(data['export_value'])
+frames['import_value'] = data['import_value']
+
+print(frames.head())
+data_normalized = preprocessing.normalize(frames, axis=0)
+print(data_normalized)
+data['export_value'] = data_normalized[:,0]
+data['import_value'] = data_normalized[:,1]
+print("data['export_value']", data['export_value'])
+print("data['import_value']", data['import_value'])
+
 location_id = pd.unique(data['location_id'])
 location_code = pd.unique(data['location_code'])
 years = pd.unique(data['year'])
