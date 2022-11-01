64b2d0ba3196c2d92432545b4f2efe26b16f8135
Arnault Pachot <apachot@openstudio.fr>
2022-11-01 15:02:11 +0100

    building json for eqch country

--- buildDynamicProductSpaceJson.py
+++ buildDynamicProductSpaceJson.py
@@ -5,106 +5,77 @@ import math
 from sklearn import preprocessing
 
 # edges
-country="AUS"
 
-datatemp = pd.read_csv('./input/exports/FRA/exports_FRA_2020.csv')
+
 country_list = pd.read_csv('./input/country_list.csv', header=None, index_col=False).values
 country_list = [i[0] for i in country_list]
-print("country_list", country_list)
-product_id = pd.unique(datatemp['product_id']).tolist()
-hs_product_code = pd.unique(datatemp['hs_product_code']).tolist()
-print("hs_product_code", hs_product_code)
-mynode_ids = pd.DataFrame(product_id, index=hs_product_code).to_dict()[0]
-#print(mynode_ids)
 
+for country in country_list:
 
+	datatemp = pd.read_csv('./input/exports/FRA/exports_FRA_2020.csv')
+	product_id = pd.unique(datatemp['product_id']).tolist()
+	hs_product_code = pd.unique(datatemp['hs_product_code']).tolist()
+	print("hs_product_code", hs_product_code)
+	mynode_ids = pd.DataFrame(product_id, index=hs_product_code).to_dict()[0]
 
-	
-#print("len(product_id)", len(product_id))
-#for i,j in myedges:
-#	print("i",i,"j",j)
-#
-#datatemp = datatemp[(datatemp['location_id'] == location_id[i]) & (data['hs_product_code'] == hs_product_code[j])]
-#print(myedges)
+	dictionary = {}
+	for k in range(0, 26):
 
+		data = pd.read_csv('./input/proximities/HS6_Proximities_0.55_'+str(1995+k)+'.csv')
+		del data['weight']
+		myedges = data.values.tolist()
+		for k2 in range(0, len(myedges)):
+			i = myedges[k2][0]
+			j = myedges[k2][1]
+		
+			if(i!='unspecified' and i!='travel' and i!='transport' and i!='financial' and i!='ict'):
+				i = str(i).zfill(6)
+			if(j!='unspecified' and j!='travel' and j!='transport' and j!='financial' and j!='ict'):
+				j = str(j).zfill(6)
+			#print("i",i,"j",j)
 
-dictionary = {}
-for k in range(0, 26):
+			index_i = hs_product_code.index(i)
+			index_j = hs_product_code.index(j)
+			code_i = product_id[index_i]-5000
+			code_j = product_id[index_j]-5000
+			if code_i >= 6000:
+				code_i = code_i - 961
+			if code_j >= 6000:
+				code_j = code_j - 961
 
-	data = pd.read_csv('./input/proximities/HS6_Proximities_0.55_'+str(1995+k)+'.csv')
-	del data['weight']
-	myedges = data.values.tolist()
-	for k2 in range(0, len(myedges)):
-		i = myedges[k2][0]
-		j = myedges[k2][1]
-	
-		if(i!='unspecified' and i!='travel' and i!='transport' and i!='financial' and i!='ict'):
-			i = str(i).zfill(6)
-		if(j!='unspecified' and j!='travel' and j!='transport' and j!='financial' and j!='ict'):
-			j = str(j).zfill(6)
-		#print("i",i,"j",j)
+			myedges[k2][0] = code_i
+			myedges[k2][1] = code_j
 
-		index_i = hs_product_code.index(i)
-		index_j = hs_product_code.index(j)
-		code_i = product_id[index_i]-5000
-		code_j = product_id[index_j]-5000
-		if code_i >= 6000:
-			code_i = code_i - 961
-		if code_j >= 6000:
-			code_j = code_j - 961
+		# weights
+		data = pd.read_csv('./input/proximities/HS6_Proximities_0.55_'+str(1995+k)+'.csv')
+		del data['target']
+		del data['source']
+		myweights = data.to_numpy().transpose()[0].tolist()
 
-		myedges[k2][0] = code_i
-		myedges[k2][1] = code_j
-	#print("index_i",code_i,"index_j",code_j)
+		print("country", country)
+		print("k", k)
+		#for year in range(1995, 2021):
+		data = pd.read_csv('./input/exports/'+country+'/exports_'+country+'_'+str(1995+k)+'.csv')
+		myFX = data['export_value'].values.tolist()
+		myFY = myFX
+		myFX = [[0] if math.isnan(x) else [x] for x in myFX]
+		myFY = [0 if math.isnan(x) else x for x in myFY]
+		print("myFX.len", len(myFX))
+		print("myedges.len", len(myedges))
+		print("myweights.len", len(myweights))
 
-	# weights
-	data = pd.read_csv('./input/proximities/HS6_Proximities_0.55_'+str(1995+k)+'.csv')
-	del data['target']
-	del data['source']
-	myweights = data.to_numpy().transpose()[0].tolist()
-
-	#print(myweights)
-
-	#for country in country_list:
-	print("country", country)
-	print("k", k)
-	#for year in range(1995, 2021):
-	data = pd.read_csv('./input/exports/'+country+'/exports_'+country+'_'+str(1995+k)+'.csv')
-	myFX = data['export_value'].values.tolist()
-	myFY = myFX
-	myFX = [[0] if math.isnan(x) else [x] for x in myFX]
-	myFY = [0 if math.isnan(x) else x for x in myFY]
-	#print("longeur=", len(myFXtmp))
+		dictionary[k]={"index": k,
+		    "edges": myedges,
+   		 	"weights": myweights,
+    		"y": myFY,
+    		"X": myFX
+    		}
 	
 
-	#print("myFX", myFX)
-	#myFX = preprocessing.normalize(myFX, axis=1)
-	#myFX = myFX.tolist()
-
-	#myFX_x = myFX[0:-1]
-	#myFX_y = myFX[1:]
-	
-	#print("myFX_x", len(myFX_x))
-	#print("myFX_y", len(myFX_y))
-	print("myFX.len", len(myFX))
-	print("myedges.len", len(myedges))
-	print("myweights.len", len(myweights))
-
-
-
-
-	dictionary[k]={"index": k,
-	    "edges": myedges,
-   	 	"weights": myweights,
-    	"y": myFY,
-    	"X": myFX
-    	}
-	
-
-dictionary["time_periods"] = 26
-dictionary["node_ids"] = mynode_ids
-#json_name = "./dataset/dynamic_productspace_"+country+".json"
-json_name = "./dataset/dynamic_productspace.json"
-print("writing", json_name, "...")
-with open(json_name, "w") as outfile:
-    json.dump(dictionary, outfile)
+	dictionary["time_periods"] = 26
+	dictionary["node_ids"] = mynode_ids
+	#json_name = "./dataset/dynamic_productspace_"+country+".json"
+	json_name = "./dataset/dynamic_productspace_"+country+".json"
+	print("writing", json_name, "...")
+	with open(json_name, "w") as outfile:
+	    json.dump(dictionary, outfile)
--- buildProductSpaceJson.py
+++ buildProductSpaceJson.py
@@ -4,85 +4,69 @@ import pandas as pd
 import math
 from sklearn import preprocessing
 
-# edges
-
-datatemp = pd.read_csv('./input/exports/FRA/exports_FRA_1995.csv')
 country_list = pd.read_csv('./input/country_list.csv', header=None, index_col=False).values
 country_list = [i[0] for i in country_list]
-print("country_list", country_list)
-product_id = pd.unique(datatemp['product_id']).tolist()
-hs_product_code = pd.unique(datatemp['hs_product_code']).tolist()
 
-mynode_ids = pd.DataFrame(product_id, index=hs_product_code).to_dict()[0]
-#print(mynode_ids)
+for country in country_list:
+
+	datatemp = pd.read_csv('./input/exports/FRA/exports_FRA_1995.csv')
+	product_id = pd.unique(datatemp['product_id']).tolist()
+	hs_product_code = pd.unique(datatemp['hs_product_code']).tolist()
+
+	mynode_ids = pd.DataFrame(product_id, index=hs_product_code).to_dict()[0]
+	#print(mynode_ids)
 
 
-data = pd.read_csv('./input/proximities/HS6_Proximities_0.55_2019.csv')
-del data['weight']
-myedges = data.values.tolist()
-for k in range(0, len(myedges)):
-	i = myedges[k][0]
-	j = myedges[k][1]
-	
-	if(i!='unspecified' and i!='travel' and i!='transport' and i!='financial' and i!='ict'):
-		i = str(i).zfill(6)
-	if(j!='unspecified' and j!='travel' and j!='transport' and j!='financial' and j!='ict'):
-		j = str(j).zfill(6)
-	#print("i",i,"j",j)
+	data = pd.read_csv('./input/proximities/HS6_Proximities_0.55_2019.csv')
+	del data['weight']
+	myedges = data.values.tolist()
+	for k in range(0, len(myedges)):
+		i = myedges[k][0]
+		j = myedges[k][1]
+		
+		if(i!='unspecified' and i!='travel' and i!='transport' and i!='financial' and i!='ict'):
+			i = str(i).zfill(6)
+		if(j!='unspecified' and j!='travel' and j!='transport' and j!='financial' and j!='ict'):
+			j = str(j).zfill(6)
+		#print("i",i,"j",j)
 
-	index_i = hs_product_code.index(i)
-	index_j = hs_product_code.index(j)
-	code_i = product_id[index_i]-5000
-	code_j = product_id[index_j]-5000
-	if code_i >= 6000:
-		code_i = code_i - 961
-	if code_j >= 6000:
-		code_j = code_j - 961
+		index_i = hs_product_code.index(i)
+		index_j = hs_product_code.index(j)
+		code_i = product_id[index_i]-5000
+		code_j = product_id[index_j]-5000
+		if code_i >= 6000:
+			code_i = code_i - 961
+		if code_j >= 6000:
+			code_j = code_j - 961
 		
 
-	myedges[k][0] = code_i
-	myedges[k][1] = code_j
-	#print("index_i",code_i,"index_j",code_j)
+		myedges[k][0] = code_i
+		myedges[k][1] = code_j
 	
-#print("len(product_id)", len(product_id))
-#for i,j in myedges:
-#	print("i",i,"j",j)
-#
-#datatemp = datatemp[(datatemp['location_id'] == location_id[i]) & (data['hs_product_code'] == hs_product_code[j])]
-#print(myedges)
+	# weights
+	data = pd.read_csv('./input/proximities/HS6_Proximities_0.55_2019.csv')
+	del data['target']
+	del data['source']
+	myweights = data.to_numpy().transpose()[0].tolist()
 
-# weights
-data = pd.read_csv('./input/proximities/HS6_Proximities_0.55_2019.csv')
-del data['target']
-del data['source']
-myweights = data.to_numpy().transpose()[0].tolist()
-
-#print(myweights)
-country_list = ['AUS']
-for country in country_list:
 	print("country", country)
 	myFX = []
 	for year in range(1995, 2021):
 		data = pd.read_csv('./input/exports/'+country+'/exports_'+country+'_'+str(year)+'.csv')
 		myFXtmp = data['export_value'].values.tolist()
 		myFXtmp = [0 if math.isnan(x) else x for x in myFXtmp]
-		#print("longeur=", len(myFXtmp))
 		myFX.append(myFXtmp)
 
-
-	#print("myFX", myFX)
-	#myFX = preprocessing.normalize(myFX, axis=0)
-	#myFX = myFX.tolist()
-	#print("myFX", myFX)
-
-
+	print("myFX.len", len(myFX))
+	print("myedges.len", len(myedges))
+	print("myweights.len", len(myweights))
 
 	dictionary = {
 	    "edges": myedges,
 	    "weights": myweights,
 	    "X": myFX
 	}
-	json_name = "./dataset/productspace.json"
+	json_name = "./dataset/productspace_"+country+".json"
 	print("writing", json_name, "...")
 	with open(json_name, "w") as outfile:
 	    json.dump(dictionary, outfile)
